75a4ba23548dcc0520d43a28da14f5ce8be1622e
Sang Tran <tony_633@hotmail.com>
2016-06-17 14:49:55 -0400

    everything before adding webpack

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 'use strict';
 
 var express = require('express');
-
+//from index.js 
+var router = require('./api');
 var app = express();
 
-app.use(express.static('public'));
+app.use('/',express.static('public'));
+
+app.use('/api', router);
+
 
 app.listen(3000,function(){
 	console.log('listening on port 3000');
