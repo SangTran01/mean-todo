fcf27fa0e899f4a8aaba782e7d7640aeca9d41e4
Sang Tran <tony_633@hotmail.com>
2016-06-18 18:31:19 -0400

    added delete method

--- public/scripts/controllers/todo.js
+++ public/scripts/controllers/todo.js
@@ -2,17 +2,29 @@
 
 angular.module('todoListApp')
 .controller('todoCtrl', function($scope, dataService) {
-  $scope.deleteTodo = function(todo, index) {
-    $scope.todos.splice(index, 1);
-    dataService.deleteTodo(todo);
+  // $scope.deleteTodo = function(todo, index) {
+  //   $scope.todos.splice(index, 1);
+  //   dataService.deleteTodo(todo);
+  // };
+  $scope.deleteTodo = function (todo, index) {
+    dataService.deleteTodo(todo).then(function () {
+        $scope.todos.splice(index, 1);
+    });
   };
-  
+
   $scope.saveTodos = function() {
     var filteredTodos = $scope.todos.filter(function(todo){
       if(todo.edited) {
         return todo
       };
     })
-    dataService.saveTodos(filteredTodos);
+    dataService.saveTodos(filteredTodos)
+    .finally($scope.resetTodoState());
   }; 
+
+  $scope.resetTodoState = function(){
+    $scope.todos.forEach(function(todo){
+      todo.edited = false;
+    });
+  };
 });
