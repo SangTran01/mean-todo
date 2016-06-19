b776b2d0dc124060ac346bc9e274509492ca267d
sang tran <tony_633@hotmail.com>
2016-06-19 14:33:32 -0400

    edited main.css

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@
 
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/mean-todo', function(err){
+mongoose.connect('mongodb://sang.tran:ttran1121@ds023432.mlab.com:23432/todo_123', function(err){
 	if(err){
 		console.log('error connecting to mongodb');
 	} else {
